2530c94ac5053a3435aad6a6c7ae0dfa8b8cf862
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-04-30 11:39:07 +0200

    test: updated main

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,29 @@
-import sys
-from src.definitions import create_models_from_json
+from src.definitions import create_models_from_json, get_functions_json
 from llm_sdk import Small_LLM_Model
 from src.utils import load_vocab
-from src.generation import generate_function
+from src.generation import generate_function, pre_prompt
+import json
+from typing import Any
+
 
 def main() -> None:
     model = Small_LLM_Model()
-    create_models_from_json("./data/input/functions_definition.json")
+    functions: list[dict[str, Any]] = get_functions_json(
+        "./data/input/functions_definition.json")
+    pydantic_models = create_models_from_json(functions)
     reversed_vocab: dict[int, str] = load_vocab(model)
-    generated_text = generate_function("Generate a simple json with key number and a number",
-                                       model, reversed_vocab)
-    print(generated_text)
-    # 
+    prompt: str = pre_prompt("What is the sum of 2 and 3?", functions)
+    generated_text = generate_function(prompt, model, reversed_vocab,
+                                       pydantic_models)
+    try:
+        print(generated_text)
+        data = json.loads(generated_text)
+        with open('data.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        print("Format JSON valide")
+    except ValueError:
+        print("Format JSON invalide")
+
     # ids = model._tokenizer.encode("build a json data object:", add_special_tokens=False)
     # for _ in range(50):
     #     logits = model.get_logits_from_input_ids(ids)
