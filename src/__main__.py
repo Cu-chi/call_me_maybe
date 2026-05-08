0e25200e44cc9b53d0b58d57db31c15afe97122c
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-05-08 16:24:05 +0200

    feat(parsing): parsing arguments

--- src/__main__.py
+++ src/__main__.py
@@ -5,14 +5,16 @@ from src.utils import load_vocab
 from src.generation import generate_function
 import json
 from typing import Any
+from src.parsing import parse
+from argparse import Namespace
 
 
 def main() -> None:
+    args: Namespace = parse()
     model = Small_LLM_Model()
     functions: list[dict[str, Any]] = get_functions_json(
-        "./data/input/functions_definition.json")
-    prompts: list[str] = get_prompts_json(
-        "./data/input/function_calling_tests.json")
+        args.functions_definition)
+    prompts: list[str] = get_prompts_json(args.input)
     pydantic_models = create_models_from_json(functions)
     reversed_vocab: dict[int, str] = load_vocab(model)
 
@@ -28,7 +30,7 @@ def main() -> None:
         except ValueError:
             missed += 1
             print(f"Invalid JSON format for prompt: '{prompt}', got: {result}")
-    with open('data/output/function_calling_results.json', 'w') as f:
+    with open(args.output, 'w') as f:
         json.dump(output, f, indent=4)
     print(f"Done generating function calling results (missed: {missed})")
 
---
+++ src/parsing.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def parse() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="specify input and output files")
+    parser.add_argument("--functions_definition",
+                        default="./data/input/functions_definition.json")
+    parser.add_argument("--input",
+                        default="./data/input/function_calling_tests.json")
+    parser.add_argument("--output",
+                        default="./data/output/function_calling_results.json")
+    args = parser.parse_args()
+    return args
