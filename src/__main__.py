823fa6e111fe8a877a1b85f90425893f9b824f85
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-05-08 17:53:59 +0200

    tweak: error handling

--- src/__main__.py
+++ src/__main__.py
@@ -1,38 +1,65 @@
 from src.definitions import create_models_from_json, \
-    get_functions_json, get_prompts_json
+    get_functions_json, get_prompts_json, SchemaError, SchemaTypeError
 from llm_sdk import Small_LLM_Model
 from src.utils import load_vocab
 from src.generation import generate_function
 import json
+import sys
+import os
 from typing import Any
 from src.parsing import parse
 from argparse import Namespace
 
 
 def main() -> None:
-    args: Namespace = parse()
-    model = Small_LLM_Model()
-    functions: list[dict[str, Any]] = get_functions_json(
-        args.functions_definition)
-    prompts: list[str] = get_prompts_json(args.input)
-    pydantic_models = create_models_from_json(functions)
-    reversed_vocab: dict[int, str] = load_vocab(model)
+    try:
+        args: Namespace = parse()
+        model = Small_LLM_Model()
+        functions: list[dict[str, Any]] = get_functions_json(
+            args.functions_definition)
+        prompts: list[str] = get_prompts_json(args.input)
+        pydantic_models = create_models_from_json(functions)
+        reversed_vocab: dict[int, str] = load_vocab(model)
+    except SchemaError as e:
+        print(f"Error: key '{e.key}' is not in the function definition",
+              file=sys.stderr)
+        return
+    except SchemaTypeError as e:
+        print(f"Error: unknown type '{e.type}' in function definition",
+              file=sys.stderr)
+        return
+    except KeyError:
+        print("Error: format error in function definition", file=sys.stderr)
+        return
+    except FileNotFoundError as e:
+        print(f"Error: {e}", file=sys.stderr)
+        return
+    except Exception as e:
+        print(f"Error: {e}")
+        return
 
     output: list[dict[str, Any]] = []
     missed: int = 0
-    for prompt in prompts:
-        print(f"Generating JSON for: '{prompt}'...")
-        result = generate_function(prompt, model, reversed_vocab,
-                                   functions, pydantic_models)
-        try:
-            data = json.loads(result)
-            output.append(data)
-        except ValueError:
-            missed += 1
-            print(f"Invalid JSON format for prompt: '{prompt}', got: {result}")
-    with open(args.output, 'w') as f:
-        json.dump(output, f, indent=4)
-    print(f"Done generating function calling results (missed: {missed})")
+    try:
+        for prompt in prompts:
+            print(f"Generating JSON for: '{prompt}'...")
+            result = generate_function(prompt, model, reversed_vocab,
+                                       functions, pydantic_models)
+            try:
+                data = json.loads(result)
+                output.append(data)
+            except ValueError:
+                missed += 1
+                print(f"Invalid JSON format, got: {result}")
+            os.makedirs(os.path.dirname(args.output), exist_ok=True)
+            with open(args.output, 'w+') as f:
+                json.dump(output, f, indent=4)
+            print(f"Done (missed: {missed})")
+    except PermissionError:
+        print(f"Error: not enought permissions for writing on '{args.output}'",
+              file=sys.stderr)
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
 
 
 if __name__ == "__main__":
--- src/definitions.py
+++ src/definitions.py
@@ -3,8 +3,16 @@ from pydantic import BaseModel, create_model
 from typing import Type, Any
 
 
+class SchemaError(Exception):
+    def __init__(self, key, *args):
+        self.key = key
+        super().__init__(*args)
+
+
 class SchemaTypeError(Exception):
-    pass
+    def __init__(self, type, *args):
+        self.type = type
+        super().__init__(*args)
 
 
 def get_python_types(type_str: str) -> Type[Any]:
@@ -16,7 +24,7 @@ def get_python_types(type_str: str) -> Type[Any]:
         return str
     elif type_str == "boolean":
         return bool
-    raise SchemaTypeError(f"type JSON '{type_str}' is not supported")
+    raise SchemaTypeError(type_str)
 
 
 def get_functions_json(path: str) -> list[dict[str, Any]]:
@@ -38,9 +46,12 @@ def create_models_from_json(functions: list[dict[str, Any]])\
      -> dict[str, Type[BaseModel]]:
     models: dict[str, Type[BaseModel]] = {}
     for func in functions:
-        func_name: str = func["name"]
-        params: dict[str, Any] = func.get('parameters', {})
-
+        func_name: str = func.get("name", None)
+        if func_name is None:
+            return SchemaError("name")
+        params: dict[str, Any] | None = func.get('parameters', None)
+        if params is None:
+            raise SchemaError("parameters")
         fields: dict[str, Any] = {
             param_name: (get_python_types(details["type"]), ...)
             for param_name, details in params.items()
