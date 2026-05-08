35f81b15b789d60e32d111e581edeaad792d5501
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-05-08 16:23:37 +0200

    tweak(generation): no replace for backslash to avoid bad variables

--- src/generation.py
+++ src/generation.py
@@ -67,7 +67,7 @@ def generate_function(input: str, llm: Small_LLM_Model,
             break
         input_ids.append(next_token_id)
         next_token_str = reversed_vocab[next_token_id]
-        generated_text += next_token_str.replace("\\", "\\\\")
+        generated_text += next_token_str
         cur_state = constrainer.update_state(cur_state, next_token_str)
         print(generated_text)
         if cur_state is None or cur_state[0] == "DONE":
