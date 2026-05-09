94e0aa1e8250893d2422b75851d90ca8911a40f9
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-05-09 14:20:09 +0200

    feat(generation): use rich to format better

--- src/generation.py
+++ src/generation.py
@@ -47,31 +47,42 @@ def generate_function(input: str, llm: Small_LLM_Model,
     max_tokens: int = 200
     constrainer = SchemaConstrainer(models, input)
     cur_state: State | None = constrainer.post_name_state()
-    for _ in range(max_tokens):
-        if cur_state is None:
-            return generated_text
-        logits: list[float] = llm.get_logits_from_input_ids(input_ids)
+    syntax = Syntax(generated_text, "json", theme="monokai", word_wrap=True)
+    panel: Panel = Panel(syntax,
+                         title="Generating...",
+                         border_style="blue")
+    with Live(panel, refresh_per_second=20) as live:
+        for _ in range(max_tokens):
+            if cur_state is None:
+                return generated_text
+            logits: list[float] = llm.get_logits_from_input_ids(input_ids)
 
-        masked_logit = [float("-inf")] * len(logits)
+            masked_logit = [float("-inf")] * len(logits)
 
-        allowed_first: str | None = constrainer.get_allowed_first_chars(
-                cur_state[0])
+            allowed_first: str | None = constrainer.get_allowed_first_chars(
+                    cur_state[0])
 
-        for token_id, token_str in reversed_vocab.items():
-            if allowed_first is not None and token_str[0] not in allowed_first:
-                continue
-            if constrainer.update_state(cur_state, token_str) is not None:
-                masked_logit[token_id] = logits[token_id]
+            for token_id, token_str in reversed_vocab.items():
+                if allowed_first is not None\
+                   and token_str[0] not in allowed_first:
+                    continue
+                if constrainer.update_state(cur_state, token_str) is not None:
+                    masked_logit[token_id] = logits[token_id]
 
-        next_token_id: int = int(np.argmax(masked_logit))
-        if masked_logit[next_token_id] == float("-inf"):
-            break
-        input_ids.append(next_token_id)
-        next_token_str = reversed_vocab[next_token_id]
-        generated_text += next_token_str
-        cur_state = constrainer.update_state(cur_state, next_token_str)
-        print(generated_text, end='\r')
-        if cur_state is None or cur_state[0] == "DONE":
-            break
+            next_token_id: int = int(np.argmax(masked_logit))
+            if masked_logit[next_token_id] == float("-inf"):
+                break
+            input_ids.append(next_token_id)
+            next_token_str = reversed_vocab[next_token_id]
+            generated_text += next_token_str
+            cur_state = constrainer.update_state(cur_state, next_token_str)
+
+            syntax.code = generated_text
+            live.update(panel)
+            if cur_state is None or cur_state[0] == "DONE":
+                panel.title = "[bold green]Generation done[/bold green]"
+                panel.border_style = "green"
+                live.update(panel)
+                break
     print()
     return generated_text
