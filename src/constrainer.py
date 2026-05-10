c0e893856f245c4704d46418dcbefc640f7135b6
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-05-10 17:59:13 +0200

    fix(generation/constrainer): block '-.' number value

--- src/constrainer.py
+++ src/constrainer.py
@@ -272,7 +272,7 @@ class SchemaConstrainer:
             if char in "0123456789.":
                 if len(buffer) > 30:
                     return None
-                if char == "." and "." in buffer:
+                if char == "." and ("." in buffer or buffer[-1] == "-"):
                     return None
                 return ("IN_NUMBER_VAL", buffer + char, func_name,
                         param_name, False, parsed_params)
